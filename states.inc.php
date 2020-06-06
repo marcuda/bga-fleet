e73d6ef42b13de01adba24792e5f5abd09905722
Dan Marcus <bga.marcuda@gmail.com>
2020-06-06 01:16:04 -0400

    Convert tabs to spaces

--- states.inc.php
+++ states.inc.php
@@ -64,12 +64,12 @@ $machinestates = array(
     // Note: ID=2 => your first state
 
     2 => array(
-    		"name" => "playerTurn",
-    		"description" => clienttranslate('${actplayer} must play a card or pass'),
-    		"descriptionmyturn" => clienttranslate('${you} must play a card or pass'),
-    		"type" => "activeplayer",
-    		"possibleactions" => array( "playCard", "pass" ),
-    		"transitions" => array( "playCard" => 2, "pass" => 2 )
+        "name" => "playerTurn",
+        "description" => clienttranslate('${actplayer} must play a card or pass'),
+        "descriptionmyturn" => clienttranslate('${you} must play a card or pass'),
+        "type" => "activeplayer",
+        "possibleactions" => array( "playCard", "pass" ),
+        "transitions" => array( "playCard" => 2, "pass" => 2 )
     ),
     
 /*
